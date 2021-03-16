bb30fed8fc3380b73cec904e7ad819d11c6e02cb
Bitchiko Kodua <bitchiko.kodua@gamesys.ee>
2021-03-16 12:02:48 +0200

    removed extras and added jwt info endpoint

--- server/src/routes/api/v1/aniamls/index.ts
+++
@@ -1,8 +0,0 @@
-import Router from "@koa/router";
-import AnimalController from "../../../../controllers/v1/AnimalController";
-
-const animalRouter = new Router();
-
-animalRouter.post('/animal/new', AnimalController.addNewAnimal);
-
-export default animalRouter;
